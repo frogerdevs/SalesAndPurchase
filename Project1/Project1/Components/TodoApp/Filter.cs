531083dc9796f0659c2a52b901612dacfd717cbd
frogerdevs <38326221+frogerdevs@users.noreply.github.com>
2023-09-20 20:16:06 +0700

    remove unnecessary project
    remove unnecessary project

--- Project1/Project1/Components/TodoApp/Filter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project1.Components.TodoApp
-{
-    public enum Filter
-    {
-        All,
-        Active,
-        Completed,
-    }
-}
--- Project1/Project1/Components/TodoApp/Todo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project1.Components.TodoApp
-{
-    public class Todo
-    {
-        public bool Completed { get; set; }
-
-        public string? Description { get; set; }
-    }
-}
